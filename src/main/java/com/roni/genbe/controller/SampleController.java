fc87c0377ead49e339f8a2eac8e7ad6d6b900e0a
Roni-Sumantri\RoniS <ronisumantri98@gmail.com>
2020-09-07 15:30:22 +0700

    post biodata, pendidikan, data table

---
+++ src/main/java/com/roni/genbe/controller/SampleController.java
@@ -0,0 +1,61 @@
+//package com.roni.genbe.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "/")
+//public class SampleController {
+//        @GetMapping("/index")
+//        public String index() {
+//            return "index";
+//        }
+//
+//        @GetMapping("/home")
+//        public String home() {
+//            return "home";
+//        }
+//
+//        @GetMapping("/biodata1")
+//        public String biodata1() {
+//            return "dashboard/biodata1";
+//        }
+//
+//        @GetMapping("/biodata2")
+//        public String biodata2() {
+//            return "fragment/biodata2";
+//        }
+//
+//        @GetMapping("/biodata3")
+//        public String biodata3() {
+//            return "fragment/biodata3";
+//        }
+//
+//        @GetMapping("/api/person")
+//        public String personDto() {
+//            return "fragment/person";
+//        }
+//
+//        @GetMapping("/biodata4")
+//        public String biodata4() {
+//            return "fragment/biodata4";
+//        }
+//
+//        @GetMapping("/biodata")
+//        public String biodata() {
+//            return "fragment/biodata";
+//        }
+//
+//        @GetMapping("/pendidikan")
+//        public String pendidikan() {
+//            return "fragment/pendidikan";
+//        }
+//
+//        @GetMapping("/tabelperson")
+//        public String tabelperson() {
+//            return "fragment/tabelPerson";
+//        }
+//
+//
+//}
---
+++ src/main/java/com/roni/genbe/controller/mvc/BaseMvcController.java
@@ -0,0 +1,25 @@
+package com.roni.genbe.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class BaseMvcController {
+
+    @GetMapping("/biodata")
+    public String biodata() {
+        return "dashboard/biodata";
+    }
+
+    @GetMapping("/pendidikan")
+    public String pendidikan() {
+        return "dashboard/pendidikan";
+    }
+
+    @GetMapping("/tabelperson")
+    public String tabelperson() {
+        return "dashboard/tabelPerson";
+    }
+}
